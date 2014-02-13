9d9760700f40e4707ca3ceb27c8b294155c0143e
Sankalp Srivastava <richi18007@gmail.com>
2014-02-13 18:11:14 +0530

    Added a few static pages and a site layout. Also , added some tests and resolved some dependencies

--- Gemfile
+++ Gemfile
@@ -8,8 +8,10 @@ gem 'sqlite3'
 
 group :development do
         gem 'rspec-rails'
+	gem 'webrat'
 end
 
 group :test do
         gem 'rspec-rails'
+	gem 'webrat'
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,16 @@ class PagesController < ApplicationController
   
   # The home action
   def home
+  	@title = "Home"
   end
 
   # The contact action 
   def contact
+  	@title = "Contact"
   end
 
+  # The about action 
+  def about 
+	@title = "About"
+  end
 end
